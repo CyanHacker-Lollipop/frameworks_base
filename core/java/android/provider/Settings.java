aab283131068a9ae00a0f2fe07d8121f07e86d3d
Men_in_black007 <sanyam.53jain@gmail.com>
2015-01-24 17:01:04 +0100

    Frameworks: port 5.0 features
    [Base]: Recents Panel Clear All Button Position
    
    Change-Id: I58686a225d62f9bb98404fc6bee30d298ef9ea1c
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
    
    [SQUASHED] [1/2] FB: Status bar Network traffic
    
    This codes are not mine an here i'm giving the proper credits
    
    Commits inside and they authors:
    @htchoi1995: omnirom/android_frameworks_base@4268d2d
    @Haus1: omnirom/android_frameworks_base@12daeeb
    @Haus1: omnirom/android_frameworks_base@017c56c
    @cristianomatos: https://github.com/crdroidandroid/android_frameworks_base/commit/6504ad84f7bb0db10543d938f24412e2d082e43e
    
    @alviteri: Added SeekBar from KK for autohide
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/msim_status_bar.xml
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    
    Change-Id: Ibaec2ba7cf84514abe825565e054507a48eb848a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2135,6 +2135,28 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
         /**
+         * Whether or not to hide the network traffic indicator when there is no activity
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Network traffic inactivity threshold (default is 10 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms)  min: 250  max: 32750  default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
          * The keyboard brightness to be used while the screen is on.
          * Valid value range is between 0 and {@link PowerManager#getMaximumKeyboardBrightness()}
          * @deprecated Use {@link android.provider.Settings.Secure#KEYBOARD_BRIGHTNESS} instead
@@ -3408,6 +3430,20 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
+		
+		/**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
 
         /**
           * Volume keys control cursor in text fields (default is 0)
